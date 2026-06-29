945ef864b4ab226610660a39592c6fdc72534833
jnorth2 <jar3dnorth51@gmail.com>
2026-06-28 23:12:34 -0700

    realsense camera stream through unicast

--- src/go2_camera/launch/camera_capture_launch.py
+++ src/go2_camera/launch/camera_capture_launch.py
@@ -6,7 +6,7 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
-ARM_IP = '239.0.0.1'
+ARM_IP = '192.168.123.100'
 
 def generate_launch_description():
     # Replace these with your camera serial numbers
